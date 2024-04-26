a782aabb41a9dce9fc94445fabc9376824f1b243
mitramango <mitramango@gmail.com>
2024-04-26 22:03:08 +0530

    added exp.sh

---
+++ btp/exp.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Array of step values
+steps=(1 2 4 8 16 32)
+
+# Array of models
+models=("mistral" "llama2" "llama2chat")
+
+# Array of optimizers
+opts=("Adam" "SGD")
+
+# Nested loop over the arrays
+for model in "${models[@]}"
+do
+   for opt in "${opts[@]}"
+   do
+      for step in "${steps[@]}"
+      do
+         # Run the experiment with the current model, optimizer, and step value
+         python run.py --model $model --lr 0.00005 --steps $step --opt $opt
+      done
+   done
+done
--- btp/setup.sh
+++ btp/setup.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
 
-conda create -n mesgd_new python=3.8.18
+conda create -n mesgd5 python=3.8.18
 
+source activate mesgd5
 
-source activate mesgd_new
-
+pip install git+https://github.com/huggingface/transformers
 
 pip install -r req.txt
 
+pip install git+https://github.com/huggingface/transformers
 
 conda install pytorch==2.2.2 torchvision==0.17.2 torchaudio==2.2.2 pytorch-cuda=11.8 -c pytorch -c nvidia
